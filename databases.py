7e209955450adf931b57385f36fe797242499e2a
kunal2206 <52152609+kunal2206@users.noreply.github.com>
2020-09-11 23:06:28 +0530

    Add files via upload

---
+++ databases.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="aws-simplified.cfsozqviv5g2.ap-south-1.rds.amazonaws.com",
+                               user="debangshudb",
+                               passwd="debangshu1994",
+                               database="business")
+
+c = mydb.cursor()
+
+
+def insert_element(emp):
+    sqlformula = "INSERT INTO employee (username,password) VALUES (%s,%s)"
+    c.execute(sqlformula, emp)
+    mydb.commit()
+
+
+def search_element(emp):
+    sqlformula = "SELECT * FROM employee WHERE username=%s"
+    c.execute(sqlformula, (emp,))
+    return c.fetchall()
+
+
